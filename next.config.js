b19fc8cfa68b3ead39d7e5cf1e00dfc3bfd36138
Paul Montoya <paulmontoya15@gmail.com>
2020-10-15 18:48:14 -0500

    proyect init

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
+const withSass = require('@zeit/next-sass')
+module.exports = withSass({
+  cssModules: true
+})
+
+const withSass = require('@zeit/next-sass');
+const withFonts = require('nextjs-fonts');
+
+module.exports = withSass(withFonts({
+  webpack(config, options) {
+   return config;
+  },
+}));
