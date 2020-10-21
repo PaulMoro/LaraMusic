0da3c71666c5919ab02b1c84c24eaaf577bfcf74
Paul Montoya <paulmontoya15@gmail.com>
2020-10-21 17:45:46 -0500

    fixing bug header

--- components/Com__HeaderProfile.js
+++ components/Com__HeaderProfile.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "../assets/style/componentStyle/com__HeaderProfile.scss";
+import "../assets/style/componentStyle/Com__HeaderProfile.scss";
 
 const HeaderProfile = () => {
   return (
