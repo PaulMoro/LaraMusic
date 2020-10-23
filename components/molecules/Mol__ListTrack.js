5d4fa19e562ee1b9b996d3eee03444fe2ca6edb5
Paul Montoya <paulmontoya15@gmail.com>
2020-10-23 15:27:41 -0500

    stile for mol_listTrcks

--- components/molecules/Mol__ListTrack.js
+++ components/molecules/Mol__ListTrack.js
@@ -35,23 +35,21 @@ const ListTrack = () => {
         </div>
       </div>
       {music.map((item) => (
-      <div class="track_list">
-        <img class="track_cover" href={item.artwork_url}/>
-        <span class="track_data">{item.permalink}</span>
-        <div class="player-component">
-          <div class="play-button paused" data-tip="Preview" data-place="top" currentitem="true">
-            <div>
-              <svg class="progress_c" width="100%" height="100%" viewBox="0 0 120 120">
-              <circle class="progress__meter" cx="60" cy="60" r="54" stroke-width="8"></circle>
-              <circle class="progress__value"></circle>
-              </svg>
-            </div>
-          </div>
-          <audio src="https://p.scdn.co/mp3-preview/d9b90990dab9d8376d05c5d4a6b7ac3e99c03763?cid=898cb61111804d2c8bf225394482e209" preload="none"></audio> 
+        <div class="track_list">
+        <div class="track_list_detail">
+          <h4 class="track_top_detail#">1</h4>
+          <h4 class="track_top_detailSong">{item.permalink}</h4>
+          <h4 class="track_top_detailArtist">{item.user.username}</h4>
         </div>
-        <a href="/#/create/3eekarcy7kvN4yt5ZFzltW?trackSource=top_tracks">
-        Create playlist
-        </a>
+        <div class="track_list_inf">
+          <h4>{item.playback_count}</h4>
+          <h4>{item.duration} seg</h4>
+          <div class="track_list_infOptions">
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+          </div>
+        </div>
       </div>
       ))}
     </div>
