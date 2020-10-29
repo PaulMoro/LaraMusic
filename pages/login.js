079633117545b4168fd999c80d080fcf72a2d002
Fabian Andres Llanos Osorio <fa.llanos@udla.edu.co>
2020-10-29 12:11:11 -0500

    Merge with Jobanag and Fixed bugs

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,15 @@
 import React from "react";
 
-<<<<<<< HEAD
 import Header from "../components/Home/HeaderPage";
-import Login from "../components/Login/Com__Login";
+import Login from "../components/Login/Login";
+// import Login from "../components/Login/Login";
 import Footer from "../components/Home/FooterPage";
-=======
-import Header from "../components/Header/Com__HeaderPage";
-import { Login } from "../components/Login/Login";
-import Footer from "../components/Home/Com__FooterPage";
->>>>>>> jobanag
 
 export const login = () => {
   return (
     <>
       <Header />
-      <Login />
+      <Login></Login>
       <Footer />
     </>
   );
