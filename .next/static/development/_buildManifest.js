db1d33f76fe6b7a5e4a83c2a1ae89c3984a622f1
Fabian Andres Llanos Osorio <fa.llanos@udla.edu.co>
2020-10-30 22:12:26 -0500

    Merge whit Jobanag

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/album":["static\u002Fchunks\u002Fpages\u002Falbum.js"],"/player":["static\u002Fchunks\u002Fpages\u002Fplayer.js"],sortedPages:["\u002F_app","\u002F_error","\u002Falbum","\u002Fplayer"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/login":["static\u002Fchunks\u002Fpages\u002Flogin.js"],"/player":["static\u002Fchunks\u002Fpages\u002Fplayer.js"],sortedPages:["\u002F_app","\u002F_error","\u002Flogin","\u002Fplayer"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> jobanag
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/album":["static\u002Fchunks\u002Fpages\u002Falbum.js"],"/login":["static\u002Fchunks\u002Fpages\u002Flogin.js"],"/next/dist/pages/_error":["static\u002Fchunks\u002Fpages\u002Fnext\u002Fdist\u002Fpages\u002F_error.js"],"/player":["static\u002Fchunks\u002Fpages\u002Fplayer.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Falbum","\u002Flogin","\u002Fnext\u002Fdist\u002Fpages\u002F_error","\u002Fplayer"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
