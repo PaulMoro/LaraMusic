e764d9c14f8d28bd5b19efcc9098002e2243afb0
Jobana Garavito Q <nellyjobana@gmail.com>
2020-11-01 23:53:29 -0500

    wellcome page

--- pages/profile.js
+++ pages/profile.js
@@ -1,24 +1,20 @@
 import React from "react";
 
 /* import componenets */
-import Header from "../components/Home/HeaderPage";
+import Header from "../components/Header/HeaderPage";
 import NavProfile from "../components/profile/Com__NavProfile.js";
 import SectionProfile from "../components/profile/Com__SectionProfile.js";
 import Footer from "../components/Home/FooterPage";
 
-// import index from "../assets/style/indexStyle";
-
-// import index from "../assets/style/indexStyle";
-
 const profile = () => {
-  return (
-    <div>
-      <Header />
-      <NavProfile />
-      <SectionProfile />
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<NavProfile />
+			<SectionProfile />
+			<Footer />
+		</div>
+	);
 };
 
 export default profile;
--- pages/wellcome.js
+++ pages/wellcome.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-import Header from "../components/Home/HeaderPage";
+import Header from "../components/Header/HeaderPage";
 import { Wellcome } from "../components/Wellcome/";
 import Footer from "../components/Home/FooterPage";
 
 export const login = () => {
-  return (
-    <>
-      <Header />
-      {/* <Wellcome /> */}
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Wellcome />
+			<Footer />
+		</>
+	);
 };
 
 export default login;
