4714324fac99b918f38b821ff97c80a1fa27ce33
Fabian Andres Llanos Osorio <fa.llanos@udla.edu.co>
2020-11-01 20:54:38 -0500

    Finis

--- pages/album.js
+++ pages/album.js
@@ -4,9 +4,7 @@ import NavHomeScreen from "../components/Player/Nav.js";
 import ContentAbum from "../components/Albums/ContentAbum";
 import Play from "../components/Play/Play";
 function Album() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
   return (
     <div className="lara">
       <HeaderHome></HeaderHome>
@@ -15,29 +13,6 @@ function Album() {
       <Play></Play>
     </div>
   );
-=======
->>>>>>> 2197ba54cb567e95795d5071f3b35cd6ab46e9c7
-	return (
-		<div className='lara'>
-			<HeaderHome></HeaderHome>
-			<NavHomeScreen></NavHomeScreen>
-			<ContentAbum></ContentAbum>
-			<Play></Play>
-		</div>
-	);
-<<<<<<< HEAD
-=======
-  return (
-    <div className="lara">
-      <HeaderHome></HeaderHome>
-      <NavHomeScreen></NavHomeScreen>
-      <ContentAbum></ContentAbum>
-      <Play></Play>
-    </div>
-  );
->>>>>>> db7b6bf7ad104f31e98a8966e15d99c882433753
-=======
->>>>>>> jobanag
->>>>>>> 2197ba54cb567e95795d5071f3b35cd6ab46e9c7
+
 }
 export default Album;
--- pages/profile.js
+++ pages/profile.js
@@ -6,22 +6,11 @@ import NavProfile from "../components/profile/Com__NavProfile.js";
 import SectionProfile from "../components/profile/Com__SectionProfile.js";
 import Footer from "../components/Home/FooterPage";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 import index from "../assets/style/indexStyle";
 
->>>>>>> db7b6bf7ad104f31e98a8966e15d99c882433753
-=======
 import index from "../assets/style/indexStyle";
 
-=======
->>>>>>> jobanag
->>>>>>> 2197ba54cb567e95795d5071f3b35cd6ab46e9c7
-=======
-
->>>>>>> paul
 const profile = () => {
   return (
     <div>
