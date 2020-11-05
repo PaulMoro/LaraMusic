5ec3cbd68f1159adf157adc53bfb2ae6fa24235c
Fabian Andres Llanos Osorio <fa.llanos@udla.edu.co>
2020-11-05 01:40:47 -0500

    Spotify Api Connect

--- components/Header/HeaderStyle.js
+++ components/Header/HeaderStyle.js
@@ -43,5 +43,27 @@ export default css.HomeStyle`
   border: none;
   text-decoration: underline;
 }
-
+.header__links a {
+  text-decoration: none;
+  margin-right: 1.5em;
+  letter-spacing: 0.2px;
+}
+.header__link--player {
+  color: white;
+  font-weight: 300;
+}
+.header__link--login {
+  color: #ffc24b;
+  border: 0.1em solid #ffc24b;
+  border-radius: 20px;
+  padding: 0.5em 1.5em;
+  font-weight: 700;
+}
+.header__link--login:hover {
+  border-color: #dd980d;
+  color: #dd980d;
+}
+.header__link--player:hover {
+  color: rgb(214, 214, 214);
+}
 `;
