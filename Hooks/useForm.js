9f7a585cab568c16ba004a35aba8b9f5fa4347fc
Fabian Andres Llanos Osorio <fa.llanos@udla.edu.co>
2020-11-07 22:37:06 -0500

    Fixing Format to Components

--- Hooks/useForm.js
+++ Hooks/useForm.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
 export const useForm = (initialState = {}) => {
-	const [values, setValues] = useState(initialState);
+  const [values, setValues] = useState(initialState);
 
-	const reset = () => {
-		setValues(initialState);
-	};
+  const reset = () => {
+    setValues(initialState);
+  };
 
-	const handleInputChange = ({ target }) => {
-		setValues({
-			...values,
-			[target.name]: target.value,
-		});
-	};
+  const handleInputChange = ({ target }) => {
+    setValues({
+      ...values,
+      [target.name]: target.value,
+    });
+  };
 
-	return [values, handleInputChange, reset];
+  return [values, handleInputChange, reset];
 };
