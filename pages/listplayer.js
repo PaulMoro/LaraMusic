e742848b00b0e858ea204d9c8638225e2ff53236
Fabian Andres Llanos Osorio <fa.llanos@udla.edu.co>
2020-11-10 17:54:03 -0500

    Merge with Fabian

--- pages/listplayer.js
+++ pages/listplayer.js
@@ -1,21 +1,16 @@
-
-import React from 'react';
-import Link from 'next/link';
-/* import componenets */
-import NavHomeScreen from '../components/Player/Nav.js';
-// import ListPlayer from "../components/Player/ListPlayer";
-import Play from '../components/Play/Play';
+import React from "react";
+import NavHomeScreen from "../components/Player/Nav.js";
+import Play from "../components/Play/Play";
 import HeaderHome from "../src/components/Player/Header";
 import NavHomeScreen from "../src/components/Player/Nav.js";
-import ListPlayer from "../src/components/Player/ListPlayer";
 import Play from "../src/components/Play/Play";
 
 const homeScreen = () => {
   return (
-    <div className='lara'>
+    <div className="lara">
       <HeaderHome />
       <NavHomeScreen />
-      <div className='containerContent'>{/* <ListPlayer /> */}</div>
+      <div className="containerContent">{/* <ListPlayer /> */}</div>
       <Play></Play>
     </div>
   );
