e5b0da875834ac544ef5ed456ed6bb6fda02898a
fercc1097 <fercc1097@gmail.com>
2020-11-10 12:15:07 -0600

    change in player layput

--- components/SongPlayer/SongPlayer.js
+++ components/SongPlayer/SongPlayer.js
@@ -1,12 +1,12 @@
-import React from "react";
-import PlayStyles from "./SongPlayerStyles";
-import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
-import "react-h5-audio-player/lib/styles.css";
+import React from 'react';
+import PlayStyles from './SongPlayerStyles';
+import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 function SongPlayer(song) {
   return (
     <>
-      <div className="player">
-        <AudioPlayer src={song.song} layout="horizontal-reverse" />
+      <div className='player'>
+        <AudioPlayer src={song.song} layout='horizontal' />
       </div>
       <style jsx PlayStyles>
         {PlayStyles}
