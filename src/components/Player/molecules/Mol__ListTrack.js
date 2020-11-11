7b065eb9a873a965d32b112cd47a428813880578
fercc1097 <fercc1097@gmail.com>
2020-11-11 16:04:45 -0600

    solving bug caused in prevous merge

--- src/components/Player/molecules/Mol__ListTrack.js
+++ src/components/Player/molecules/Mol__ListTrack.js
@@ -29,80 +29,44 @@ const ListTrack = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className='track'>
-      <h2>Top Tracks</h2>
-      <div className='tab__top'>
-        <p className='tab__top__number'>#</p>
-        <p className='tab__top__tittel'>title</p>
-        <p className='tab__top__plays'>Daily plays</p>
-        <p className='tab__top__time'>Time</p>
-        <p className='tab__top__option'>Options</p>
-      </div>
-      {music.map((item) => (
-        <div className='tab__music'>
-          <div className='tab__music__icons'>
-            <p>1</p>
-            <div className='icon'>
-              <CgPlayButtonO
-                onClick={() => {
-                  onPlay(item);
-                }}
-              ></CgPlayButtonO>
-            </div>
-            <div className='icon'>
-              <LikeButton user={user} song={item}></LikeButton>
-            </div>
-            <div className='icon'>
-              <GrAddCircle></GrAddCircle>
-            </div>
-          </div>
-          <p className='tab__music__tittel'>{item.title.substr(0, 12)}</p>
-          <p className='tab__music__plays'>{item.views}</p>
-          <p className='tab__music__time'>{item.duration}</p>
-          <div className='tab__music__icon'>
-            <VscEllipsis></VscEllipsis>
-          </div>
-        </div>
-      ))}
-=======
-    <section className="about__album">
-      <div className="content__music">
+    <section>
+      <div className='track'>
         <h2>Top Tracks</h2>
-        <div className="tab__top">
-          <p className="tab__top__number">#</p>
-          <p className="tab__top__tittel">title</p>
-          <p className="tab__top__plays">Daily plays</p>
-          <p className="tab__top__time">Time</p>
-          <p className="tab__top__option">Options</p>
+        <div className='tab__top'>
+          <p className='tab__top__number'>#</p>
+          <p className='tab__top__tittel'>title</p>
+          <p className='tab__top__plays'>Daily plays</p>
+          <p className='tab__top__time'>Time</p>
+          <p className='tab__top__option'>Options</p>
         </div>
-        {music.map((item) => (
-          <div className="tab__music">
-            <div className="tab__music__icons">
-              <p>1</p>
-              <div className="icon">
-                <CgPlayButtonO
-                  onClick={() => {
-                    onPlay(item);
-                  }}
-                ></CgPlayButtonO>
+        {music.map((item) => {
+          return (
+            <div className='tab__music'>
+              <div className='tab__music__icons'>
+                <p>1</p>
+                <div className='icon'>
+                  <CgPlayButtonO
+                    onClick={() => {
+                      onPlay(item);
+                    }}
+                  ></CgPlayButtonO>
+                </div>
+                <div className='icon'>
+                  <LikeButton user={user} song={item}></LikeButton>
+                </div>
+                <div className='icon'>
+                  <GrAddCircle></GrAddCircle>
+                </div>
               </div>
-              <div className="icon">
-                <FaRegThumbsUp></FaRegThumbsUp>
-              </div>
-              <div className="icon">
-                <GrAddCircle></GrAddCircle>
+              <p className='tab__music__tittel'>{item.title.substr(0, 12)}</p>
+              <p className='tab__music__plays'>{item.views}</p>
+              <p className='tab__music__time'>{item.duration}</p>
+              <div className='tab__music__icon'>
+                <VscEllipsis></VscEllipsis>
               </div>
             </div>
-            <p className="tab__music__tittel">{item.songName.substr(0, 12)}</p>
-            <p className="tab__music__plays">{item.songPlaysCount}</p>
-            <p className="tab__music__time">{item.songDuration}</p>
-            <div className="tab__music__icon">
-              <VscEllipsis></VscEllipsis>
-            </div>
-          </div>
-        ))}
->>>>>>> c9f76c70f7d174f3c2b69ffd8c4480d65876c541
+          );
+        })}
 
         <style jsx ListTrackStyle>
           {ListTrackStyle}
